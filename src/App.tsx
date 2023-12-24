5808867ff1d28d587bb488eedd3b62461a6a1769
Giovanni Di Rosa <giodirosaartes@gmail.com>
2023-12-24 18:37:26 -0300

    curso de react con typescript con Fernando Herrera

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+const App = () => {
+  return (
+    <>
+      <h1>Counter: + typescript</h1>
+      <hr />
+      <Counter />
+    </>
+  );
+};
+
+export default App;
